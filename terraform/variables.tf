feb30217d92cbde686e7f63efbefd2019eb4b73d
Fidelis Ikoroje <fidelis.ikoroje@gmail.com>
2025-07-29 16:46:01 +0100

    Project setup

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,6 +6,7 @@ variable "domain_name" {
 variable "bucket_name" {
   description = "S3 bucket name for website hosting"
   type        = string
+  default     = "portfolio-site-bucket"
 }
 
 variable "certificate_domain" {
